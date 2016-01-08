c305162c37dbd10d2ddb4bac1af667f655446876
Raajay Viswanathan <raajay.v@gmail.com>
2016-01-08 01:11:24 -0500

    fix the load script to use ENV variables

--- load_hdfs.sh
+++ load_hdfs.sh
@@ -2,4 +2,4 @@
 VERSION=tez-0.8.0-SNAPSHOT
 
 ${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /apps/${VERSION}
-${HADOOP_HOME}/bin/hdfs dfs -copyFromLocal /users/raajay86/code-netopt/tez/tez-dist/target/${VERSION}.tar.gz /apps/${VERSION}
+${HADOOP_HOME}/bin/hdfs dfs -copyFromLocal ${CODE_DIR}/tez/tez-dist/target/${VERSION}.tar.gz /apps/${VERSION}
