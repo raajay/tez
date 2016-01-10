594bc94ac781c1f832cb1a8bb1219c2ac750a695
Raajay Viswanathan <raajay.v@gmail.com>
2016-01-10 04:08:03 -0500

    Update the script to load the tez jars

--- load_hdfs.sh
+++ load_hdfs.sh
@@ -2,4 +2,5 @@
 VERSION=tez-0.8.0-SNAPSHOT
 
 ${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /apps/${VERSION}
+${HADOOP_HOME}/bin/hdfs dfs -rm /apps/${VERSION}/${VERSION}.tar.gz
 ${HADOOP_HOME}/bin/hdfs dfs -copyFromLocal ${CODE_DIR}/tez/tez-dist/target/${VERSION}.tar.gz /apps/${VERSION}
